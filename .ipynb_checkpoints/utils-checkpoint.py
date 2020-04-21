582449ceb83a7f712efb3de46234d84652c5ce47
AlbertMillan <millana@tcd.ie>
2020-04-22 03:22:37 +0800

    Implemented ResNet

--- .ipynb_checkpoints/utils-checkpoint.py
+++ .ipynb_checkpoints/utils-checkpoint.py
@@ -1,6 +1,8 @@
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 import torch
+
+import sys
 
 class ConvBlock(nn.Module):
     """ Block consisting of convolutions, batchnorm & relu (if any) layers"""
@@ -8,8 +10,9 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
         
         self.conv = nn.Conv2d(in_plane, out_plane, kernel_size=kernel, stride=stride, padding=pad, bias=bias)
-        self.bn = nn.BatchNorm(out_plane)
-        
+        self.bn = nn.BatchNorm2d(out_plane)
+    
+        self.relu = None
         if is_relu:
             self.relu = nn.ReLU(inplace=True)
         
@@ -27,6 +30,7 @@ class ConvBlock(nn.Module):
         
 
 class BasicBlock(nn.Module):
+    expansion = 1
     
     def __init__(self, in_plane, out_plane, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
@@ -42,7 +46,7 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         
         if self.downsample:
-            redidual = self.downsample(x)
+            residual = self.downsample(x)
             
         out += residual
         out = F.relu(out)
@@ -51,13 +55,33 @@ class BasicBlock(nn.Module):
     
     
 class Bottleneck(nn.Module):
+    expansion = 4
     
-    def __init__(self, in_planes, out_planes):
+    def __init__(self, in_plane, out_plane, stride=1, downsample=None):
         super(Bottleneck, self).__init__()
+        self.conv1 = ConvBlock(in_plane, out_plane, kernel=1, stride=1, pad=0, bias=False)
+        self.conv2 = ConvBlock(out_plane, out_plane, kernel=3, stride=stride, pad=1, bias=False)
+        self.conv3 = ConvBlock(out_plane, out_plane * 4, kernel=1, pad=0, is_relu=False, bias=False)
+        self.downsample = downsample
         
         
     def forward(self, x):
+        residual = x
         
+#         print(">>> Forward:", x.size())
+        
+        out = self.conv1(x)
+        out = self.conv2(out)
+        out = self.conv3(out)
+        
+        if self.downsample:
+#             print(x.size())
+            residual = self.downsample(x)
+#             print(out.size())
+#             print(residual.size())
+            
+        out += residual
+        out = F.relu(out)
         
         return out
     
--- retinanet.py
+++ retinanet.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 from utils import ConvBlock, BasicBlock, Bottleneck
 
-import sys
+import sys, os
 
 class FPN(nn.Module):
     
@@ -20,7 +20,7 @@ class RetinaNet(nn.Module):
     """
     
     def __init__(self):
-        super(FPN, self).__init__()
+        super(RetinaNet, self).__init__()
         
     def forward(self):
         pass
@@ -29,40 +29,52 @@ class RetinaNet(nn.Module):
         
 class ResNet(nn.Module):
     
-    def __init__(self, layers, block):
-        super(FPN, self).__init__()
+    def __init__(self, layers, block, num_classes):
+        super(ResNet, self).__init__()
         
-        print(">>> In ResNet...")
+        self.in_plane = 64
         
         self.conv1 = ConvBlock(3, 64, kernel=7, stride=2, pad=3, bias=False)
         
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         
-        self.conv_2 = self._make_layers(self, block, layers[0], 64, 64)
+        self.conv_2 = self._make_layers(block, layers[0], 64)        
+        self.conv_3 = self._make_layers(block, layers[1], 128, stride=2)
+        self.conv_4 = self._make_layers(block, layers[2], 256, stride=2)
+        self.conv_5 = self._make_layers(block, layers[3], 512, stride=2)
         
-        sys.exit()
-        
-        self.conv_3 = self._make_layers(self, block, layers[1], 64, 128)
-        self.conv_4 = self._make_layers(self, block, layers[2], 128, 256)
-        self.conv_5 = self._make_layers(self, block, layers[3], 256, 512)
+        self.avgpool = nn.AvgPool2d(kernel_size=7)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
         
         
-    def _make_layers(self, block, n_layers, in_plane, out_plane, stride=1):
+    def _make_layers(self, block, n_layers, out_plane, stride=1):
         
         downsample = None
         
         # Assume downsampling is performed by adding an extra layer to perform downsampling
-        if in_plane != out_plane:
-            downsample = ConvBlock(out_plane, out_plane, kernel=1, stride=stride, bias=False, is_relu=False)
+        if self.in_plane != out_plane or block == Bottleneck:
+            downsample = ConvBlock(self.in_plane, out_plane * block.expansion, kernel=1, stride=stride, pad=0, bias=False, is_relu=False)
         
-        layers = [ block(in_plane, out_plane, stride, downsample) ]
+        layers = [ block(self.in_plane, out_plane, stride, downsample) ]
+        self.in_plane = out_plane * block.expansion
         for i in range(1, n_layers):
-            layers.append( block(in_plane, out_plane) )
+            layers.append( block(self.in_plane, out_plane) )
             
-        return nn.Sequential(layers)
+        return nn.Sequential(*layers)
         
-    def forward(self):
-        pass
+        
+    def forward(self, x):
+        
+        out = self.conv1(x)
+        out = self.maxpool(out)
+        out = self.conv_2(out)
+        out = self.conv_3(out)
+        out = self.conv_4(out)
+        out = self.conv_5(out)
+        out = self.avgpool(out).view(x.size(0), -1)
+        out = self.fc(out)
+        
+        return out
     
     
 if __name__ == '__main__':
@@ -73,4 +85,5 @@ if __name__ == '__main__':
     x = torch.randn((10, 3, 224, 224)).cuda()
     layers = [2, 3, 6, 4]
     
-    model = ResNet(layers, BasicBlock).cuda()
+    model = ResNet(layers, Bottleneck, 1000).cuda()
+    model.forward(x)
